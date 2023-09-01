04acb3f976808b676305ff1b473b17425f56464a
Mariam Samy Badr <mariamsamy.1122001@gmail.com>
2023-09-02 00:32:15 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/posts', [PostController::class,'index']);
+Route::get('/posts/create', [PostController::class,'create']);
+Route::get('/posts/{id}', [PostController::class,'show']);
+Route::post('/posts', [PostController::class,'store']);
+Route::get('/posts/{id}/edit', [PostController::class,'edit']);
+Route::put('/posts/{id}', [PostController::class,'update']);
+Route::delete('/posts/{id}', [PostController::class,'destroy']);
