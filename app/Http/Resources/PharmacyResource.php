e66cfea65749072c575dd2b10c3083aee21c95db
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2023-09-11 22:22:49 +0300

    add PharmacyResource

---
+++ app/Http/Resources/PharmacyResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PharmacyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'pharmacy_name'=>$this->pharmacy_name,
+            'pharmacy_address'=>$this->pharmacy_name,
+            'pharmacy_image'=>$this->pharmacy_name,
+            'pharmacy_phone'=>$this->pharmacy_name
+        ];
+    }
+}
