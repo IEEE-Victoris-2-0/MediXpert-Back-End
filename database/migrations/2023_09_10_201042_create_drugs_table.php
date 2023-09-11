f8b0b8ffa3ad5db87dcad90c00e44fa0e532a92b
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2023-09-11 21:57:20 +0300

    edit Drugs table

---
+++ database/migrations/2023_09_10_201042_create_drugs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('drugs', function (Blueprint $table) {
+            $table->id();
+            $table->string("drug_name");
+            $table->Text("description");
+            $table->string("drug_image")->nullable()->default('default.jpg');
+            $table->decimal("item_price",5,3);
+            $table->integer("qty");
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreignId('pharmacy_id');
+            $table->foreign('pharmacy_id')->references('id')->on('pharmacies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('drugs');
+    }
+};
